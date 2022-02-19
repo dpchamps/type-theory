0905ae956c35c9f35dcba883908d4fa76a7d3959
David <david.campion@walmartlabs.com>
2022-02-19 11:37:22 -0800

    Parsing mostly tested for happy paths

--- impls/tapl/crates/full-untyped-lambda-calculus/src/parser/mod.rs
+++ impls/tapl/crates/full-untyped-lambda-calculus/src/parser/mod.rs
@@ -55,17 +55,6 @@ mod tests {
     }
 
     #[test]
-    fn test_parser_garbage() {
-        let mut context = Context::default();
-
-        match parser::parse("ues \"std\";") {
-            Err(ParseError::UnrecognizedToken { .. }) => {}
-            Err(x) => panic!("Recieved unexpected result {:?}", x),
-            Ok(x) => panic!("Recieved unexpected result {:?}", x),
-        }
-    }
-
-    #[test]
     fn test_bind() {
         let (commands, context) = parser::parse("let x;").expect("");
 
@@ -138,4 +127,149 @@ mod tests {
         let (commands, context) = parser::parse(input).unwrap();
         assert_eq!(commands, expectation);
     }
+
+    #[test]
+    fn test_atomic(){
+        let input = r#"
+        let t = true;
+        let f = false;
+        let i = 1;
+        let f = 1.0;
+        let r = {};
+        let s = "hello";
+        let v = y;
+        "#;
+
+        let (commands, _) = parser::parse(input).expect("");
+
+        assert_eq!(commands, [
+            Command::Bind(
+                FileInfo::default(),
+                Binding::TermBind(
+                    Box::new(
+                        Term::True(
+                            FileInfo::default()
+                        )
+                    )
+                )
+            ),
+            Command::Bind(
+                FileInfo::default(),
+                Binding::TermBind(
+                    Box::new(
+                        Term::False(
+                            FileInfo::default()
+                        )
+                    )
+                )
+            ),
+            Command::Bind(
+                FileInfo::default(),
+                Binding::TermBind(
+                    Box::new(
+                        Term::Successor(
+                            FileInfo::default(),
+                            Box::new(
+                                Term::Zero(
+                                    FileInfo::default()
+                                )
+                            )
+                        )
+                    )
+                )
+            ),
+            Command::Bind(
+                FileInfo::default(),
+                Binding::TermBind(
+                    Box::new(
+                        Term::Float(
+                            FileInfo::default(),
+                            1.0
+                        )
+                    )
+                )
+            ),
+            Command::Bind(
+                FileInfo::default(),
+                Binding::TermBind(
+                    Box::new(
+                        Term::Record(
+                            FileInfo::default(),
+                            vec![]
+                        )
+                    )
+                )
+            ),
+            Command::Bind(
+                FileInfo::default(),
+                Binding::TermBind(
+                    Box::new(
+                        Term::String(
+                            FileInfo::default(),
+                            String::from("hello")
+                        )
+                    )
+                )
+            ),
+            Command::Bind(
+                FileInfo::default(),
+                Binding::TermBind(
+                    Box::new(
+                        Term::Var(
+                            FileInfo::default(),
+                            Var::default()
+                        )
+                    )
+                )
+            )
+        ])  
+    }
+
+    #[test]
+    fn test_record(){
+        let input = r#"
+        let x = {
+            x = "hello",
+            y = 420.69
+        };
+        "#;
+
+        let (commands, _) = parser::parse(input).expect("Failed to parse");
+
+        assert_eq!(
+            commands,
+            [
+                Command::Bind(
+                    FileInfo::default(),
+                    Binding::TermBind(
+                        Box::new(
+                            Term::Record(
+                                FileInfo::default(),
+                                vec![
+                                    (
+                                        "x".into(), 
+                                        Box::new(
+                                            Term::String(
+                                                FileInfo::default(),
+                                                "hello".into()
+                                            )
+                                        )
+                                    ),
+                                    (
+                                        "y".into(), 
+                                        Box::new(
+                                            Term::Float(
+                                                FileInfo::default(),
+                                                420.69
+                                            )
+                                        )
+                                    )
+                                ]
+                            )
+                        )
+                    )
+                )
+            ]
+        )
+    } 
 }
